a0bc71e60ac8f344082f93b48c0747fa4735a80a
ZengJiang <mhisf@vip.qq.com>
2017-11-04 01:40:06 +0800

    First Commit

---
+++ application/api/controller/Entry.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dizy
+ * Date: 2017/11/4
+ * Time: 1:29
+ */
+
+namespace app\controller;
+
+
+use app\base\controller\Api;
+
+class Entry extends Api
+{
+    
+}
---
+++ application/base/controller/Api.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dizy
+ * Date: 2017/11/4
+ * Time: 1:30
+ */
+
+namespace app\base\controller;
+
+
+use think\Controller;
+
+class Api extends Controller
+{
+    protected function _initialize()
+    {
+
+    }
+
+}
